47559a3aa2602874ccc776d8c8020cc17b23e36a
kyj980303 <rladuswjd0303@naver.com>
2024-03-25 02:07:48 +0900

    Chore: modify jest config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ const config = {
   verbose: true,
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transformIgnorePatterns: ["node_modules/(?!(react-bootstrap-tagsinput))"],
 };
 
 module.exports = config;
